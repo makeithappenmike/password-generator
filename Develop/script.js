0d76c0aefb03c01473fa7f67f069e925f7376eac
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-14 07:35:32 -0400

    initial commit and working on prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+alert("You'd like a new password, eh? Let's define the criteria! Smash 'OK' to proceed.");
+let length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+let lowercase = prompt("Would you like to require a lowercase character? (Yes/No)");
+let uppercase = prompt("Would you like to require an uppercase character? (Yes/No)");
+let numeric = prompt("Would you like to require a numeric character? (Yes/No)");
+let special = prompt("Would you like to require a special character i.e, *, !, %, etc..? (Yes/No)");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
