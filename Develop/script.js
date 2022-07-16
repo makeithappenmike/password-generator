fc239ac94540879ad91f90fd9eded5bc568ad423
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-16 15:45:10 -0400

    working on uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -18,16 +18,67 @@ function genPass() {
 
   // Lowercase character?
   let lowercase = prompt("Would you like to require a lowercase character? (y/n)");
+
+  // While the answer is not y, Y, n, or N, don't proceed
+  while (lowercase !== "y" || lowercase !== "Y" || lowercase !== "n" || lowercase !== "N") {
+    alert("Whoops! Please choose Yes or No (y/n)");
+    let lowercase = prompt("Would you like to require a lowercase character? (y/n)");
+    if (lowercase === "y" || lowercase === "Y" || lowercase === "n" || lowercase === "N") {
+      break;
+    }
+  };
+
+  // Uppercase character?
+  let uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
+
+  // While the answer is not y, Y, n, or N, don't proceed
+  while (uppercase !== "y" || uppercase !== "Y" || uppercase !== "n" || uppercase !== "N") {
+    alert("Whoops! Please choose Yes or No (y/n)");
+    let uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
+    if (uppercase === "y" || uppercase === "n" || uppercase === "y" || uppercase === "n") {
+      break;
+    }
+  };
+
+  alert("Sick! Smash 'OK' to view your password.");
  
-   // Uppercase character?
-  let uppercase = prompt("Would you like to require an uppercase character? (y/n))");
+//    // Uppercase character?
+//   let uppercase = prompt("Would you like to require an uppercase character? (y/n))");
+
+//   // While the length is not between 8 - 128, don't proceed
+//   while (uppercase !== "y" || uppercase !== "n") {
+//     alert("Whoops! Please enter a number between 8 - 128 and try again.");
+//     let uppercase = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+//     if (uppercase === "y" || uppercase === "n") {
+//       break;
+//     }
+//   };
  
-//  Numeric character?
-  let numeric = prompt("Would you like to require a numeric character? (y/n)");
+// //  Numeric character?
+//   let numeric = prompt("Would you like to require a numeric character? (y/n)");
+
+//   // While the length is not between 8 - 128, don't proceed
+//   while (numeric !== "y" || numeric !== "n") {
+//     alert("Whoops! Please enter a number between 8 - 128 and try again.");
+//     let numeric = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+//     if (numeric === "y" || numeric === "n") {
+//       break;
+//     }
+//   };
   
-  // Special character?
-  let special = prompt("Would you like to require a special character i.e, *, !, %, etc..? (y/n)");
-}
+//   // Special character?
+//   let special = prompt("Would you like to require a special character i.e, *, !, %, etc..? (y/n)");
+// }
+
+// // While the length is not between 8 - 128, don't proceed
+// while (length < 8 || length > 128) {
+//   alert("Whoops! Please enter a number between 8 - 128 and try again.");
+//   let length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+//   if (length > 8 && length < 128) {
+//     break;
+//   }
+
+};
 
 
 // Get references to the #generate element
