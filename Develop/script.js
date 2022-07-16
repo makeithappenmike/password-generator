08a1e9b0b96693b752c188f43d9c577e098f97e0
MakeithappenMike <omichaelaaron@gmail.com>
2022-07-16 16:55:26 -0400

    conditionals are working

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,14 @@ function genPass() {
   alert("You'd like a new password, eh? Let's define the criteria! Smash 'OK' to proceed.");
   
   // Length between 8 and 128
-  let length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+  var length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
 
   // While the length is not between 8 - 128, don't proceed
   while (length < 8 || length > 128) {
     // console.log("Need correction");
     alert("Whoops! Please enter a number between 8 - 128 and try again.");
-    let length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
-    console.log(length, "is not between 8-128");
+    length = prompt("How long would like your new password to be? Please choose a numeric value between at least 8 characters and no more than 128 characters");
+    // console.log(length, "is not between 8-128");
     if (length >= 8 && length <= 128) {
       // console.log("length set to", length);
       break;
@@ -25,14 +25,14 @@ function genPass() {
   console.log("Length:", length);
 
   // Lowercase character?
-  let lowercase = prompt("Would you like to require a lowercase character? (y/n)");
+  var lowercase = prompt("Would you like to require a lowercase character? (y/n)");
 
   // While the answer is not y, Y, n, or N, don't proceed
   while (!yesNo.includes(lowercase)) {
     // console.log("Need correction");
     alert("Whoops! Please choose Yes or No (y/n)");
-    let lowercase = prompt("Would you like to require a lowercase character? (y/n)");
-    console.log(lowercase, "is not an option");
+    lowercase = prompt("Would you like to require a lowercase character? (y/n)");
+    // console.log(lowercase, "is not an option");
     if (lowercase === "y" || lowercase === "Y" || lowercase === "n" || lowercase === "N") {
       // console.log("lowercase set to", lowercase);
       break;
@@ -41,14 +41,14 @@ function genPass() {
   console.log("Lowercase:", lowercase);
 
   // Uppercase character?
-  let uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
+  var uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
 
   // While the answer is not y, Y, n, or N, don't proceed
-  while (!yesNo.includes(lowercase)) {
+  while (!yesNo.includes(uppercase)) {
     // console.log("Need correction");
     alert("Whoops! Please choose Yes or No (y/n)");
-    let uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
-    console.log(uppercase, "is not an option");
+    uppercase = prompt("Would you like to require an Uppercase character? (y/n)");
+    // console.log(uppercase, "is not an option");
     if (uppercase === "y" || uppercase === "Y" || uppercase === "n" || uppercase === "N") {
       // console.log("uppercase set to", uppercase);
       break;
@@ -57,15 +57,15 @@ function genPass() {
   console.log("Uppercase:", uppercase);
 
   // Numeric character?
-  let numeric = prompt("Would you like to require a numeric character? (y/n)");
+  var numeric = prompt("Would you like to require a numeric character? (y/n)");
   console.log("Numeric:", numeric);
 
   // While the answer is not y, Y, n, or N, don't proceed
-  while (!yesNo.includes(lowercase)) {
+  while (!yesNo.includes(numeric)) {
     // console.log("Need correction");
     alert("Whoops! Please choose Yes or No (y/n)");
-    let numeric = prompt("Would you like to require a numeric character? (y/n)");
-    console.log(numeric, "is not an option");
+    numeric = prompt("Would you like to require a numeric character? (y/n)");
+    // console.log(numeric, "is not an option");
     if (numeric === "y" || numeric === "Y" || numeric === "n" || numeric === "N") {
       // console.log("numeric set to", numeric);
       break;
@@ -73,23 +73,21 @@ function genPass() {
   };
 
   // Special character?
-  let special = prompt("Would you like to require a special character? (y/n)");
+  var special = prompt("Would you like to require a special character? (y/n)");
 
   // While the answer is not y, Y, n, or N, don't proceed
-  while (!yesNo.includes(lowercase)) {
+  while (!yesNo.includes(special)) {
     console.log("Need correction");
     alert("Whoops! Please choose Yes or No (y/n)");
-    let special = prompt("Would you like to require a special character? (y/n)");
-    console.log(special, "is not an option");
+    special = prompt("Would you like to require a special character? (y/n)");
+    // console.log(special, "is not an option");
     if (special === "y" || special === "Y" || special === "n" || special === "N") {
       // console.log("special set to", special);
       break;
     }
   };
-  console.log("Special:", special);
-
-  alert("Sick! Smash 'OK' to view your password.");
-  
+  console.log("Special:", special + "/n");
+  console.log("-------------------");
   console.log("Length:", length);
   console.log("Lowercase:", lowercase);
   console.log("Uppercase:", uppercase);
@@ -97,6 +95,7 @@ function genPass() {
   console.log("Special:", special);
 
 
+  alert("Sick! Smash 'OK' to view your password.");
 
 };
 
